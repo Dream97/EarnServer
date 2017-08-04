f0a6a064843f6badc7d3e9a18fb704c45d0cdec1
Dream97 <1248310717@qq.com>
2017-08-04 16:29:15 +0800

    完成服务器的配置

--- src/util/DBHelper.java
+++ src/util/DBHelper.java
@@ -8,12 +8,12 @@ public class DBHelper {
 	//Database Driver
 	private static final String driver= "com.mysql.jdbc.Driver";
 	//URLaddress for database connection
-	private static final String url = "jdbc:mysql://localhost:3306/make_money";
+	private static final String url = "jdbc:mysql://localhost:3306/earn";
 
 	//Username of database
-    private static final String username = "Dream97";
+    private static final String username = "root";
     //password of database
-    private static final String password = "1248310717";
+    private static final String password = "mysql2017";
 
     private static Connection conn = null;
     
